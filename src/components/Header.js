e0da2316ca7cf39649282b867c463ddd6aa431e4
Kyle Merritt <kam03m@gmail.com>
2023-06-10 18:05:54 -0400

    updated header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
 import Link from "next/link";
 
 export default function Header() {
-  return (
-    <header className="header">
-     <Link href="/">
-      <div className="logo-container">
-        <img
-          src="/qrioheaderlogo.png"
-          alt="Your logo"
-          className="logo-container"
-          width={200}
-          height={55}
-        />
-      </div>
-      </Link>
+    return (
+        <header className="header">
+            <div className="logo-container">
+                <Link href="/">
+                    <img
+                        src="/qrioheaderlogo.png"
+                        alt="Your logo"
+                        className="logo-container"
+                        width={200}
+                        height={55}
+                    />
+                </Link>
+            </div>
 
-      <Link href="/">Home</Link>
-
-      <Link href="/">Log In</Link>
-
-      <Link href="/">Register</Link>
-    </header>
-  );
+            <div className="links-container">
+                <Link href="/">Home</Link>
+                <Link href="/">Log In</Link>
+                <Link href="/">Register</Link>
+            </div>
+        </header>
+    );
 }
