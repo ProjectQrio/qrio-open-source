b7c18d7d15397b417f1dce909b9a62a69573f650
Shebechan <originalstacia@gmail.com>
2023-06-23 15:44:46 -0400

    updated with lost pages

---
+++ src/components/profile/user-profile.js
@@ -0,0 +1,32 @@
+
+import ProfileForm from "./profile-form"
+
+function UserProfile() {
+  
+async function changePasswordHandler(passwordData) {
+  const response = await fetch('/api/auth/change-password', {
+    method: 'PATCH',
+    body: JSON.stringify(passwordData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const data = await response.json();
+
+  console.log(data);
+}
+
+
+  return (
+    <div>
+      <center>
+        <h1>My profile</h1>
+        < br />   < br />   < br />   < br />
+        <h3>Change password</h3>
+        <ProfileForm onChangePassword={changePasswordHandler}></ProfileForm>
+      </center>
+    </div>
+  );
+}
+
+export default UserProfile;
