10b64112f07ba7a9d8480737ec9e037d1608d34d
Shebechan <originalstacia@gmail.com>
2023-08-24 11:16:09 -0700

    tried to add Google Analytics. Not sure if it's working or not

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
-// pages/_app.js
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import ReactGA from "react-ga";
 
 export default function App({ Component, pageProps }) {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      ReactGA.initialize("G-HBW52341QW");
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, []);
+
   return (
     <UserProvider>
       <Component {...pageProps} />
