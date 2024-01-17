3d1052e2da34eec4e2b864d8d07a721edf4b67cc
Shebechan <originalstacia@gmail.com>
2024-01-16 20:58:57 -0800

    Mailchimp script was in the wrong place, tried fixing location to Head tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,8 +22,7 @@ class MyDocument extends Document {
       
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
 
-</Head>
-<script id="mcjs">
+  <script id="mcjs">
   {!function(c,h,i,m,p){
     m=c.createElement(h);
     p=c.getElementsByTagName(h)[0];
@@ -32,6 +31,8 @@ class MyDocument extends Document {
     p.parentNode.insertBefore(m,p);
   }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/35112925652b0562a3805d40b/dba383321afcfbcb527e92a11.js")}
 </script>
+
+</Head>
       <body>
         <Main />
         <NextScript />
