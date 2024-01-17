f955efaca2c9d95e622861d87982dabc6ab4087a
Shebechan <originalstacia@gmail.com>
2024-01-16 20:43:06 -0800

    Adding Mailchimp Subscriber Pop Up

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,6 +23,15 @@ class MyDocument extends Document {
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
 
 </Head>
+<script id="mcjs">
+  {!function(c,h,i,m,p){
+    m=c.createElement(h);
+    p=c.getElementsByTagName(h)[0];
+    m.async=1;
+    m.src=i;
+    p.parentNode.insertBefore(m,p);
+  }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/35112925652b0562a3805d40b/dba383321afcfbcb527e92a11.js")}
+</script>
       <body>
         <Main />
         <NextScript />
